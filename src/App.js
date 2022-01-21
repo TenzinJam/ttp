8e3ca962c7ad72d21ec2eba88b2813c92edef22d
Tenzin Jamyang <jamyangu@gmail.com>
2022-01-21 09:57:59 -0500

    added form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Teachers from "./components/Teachers"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import TeacherForm from './components/TeacherForm';
 
 function App() {
   return (
-   <Teachers />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/teachers" element={<Teachers />}></Route>
+          <Route path="/teacherForm" element={<TeacherForm/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <h1>Welcome to Demo</h1>
+    </div>
+
   );
 }
 
---
+++ src/components/TeacherForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import Axios from "axios"
+export default function TeacherForm(){
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  async function submitHandler(e){
+    e.preventDefault()
+    await Axios.post(`http://localhost:8080/api/teachers`, {firstName, lastName})
+  }
+
+
+  return (
+    <div>
+      <h1>Add a teacher to the Registry</h1>
+      <form onSubmit={submitHandler}>
+        <label>
+          First Name:
+          <input type="text" name="firstName" value={firstName} onChange={setFirstName(firstName)}/>
+        </label>
+        <label>
+          Last Name:
+          <input type="text" name="lastName" value={lastName} onChange={setLastName(lastName)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
