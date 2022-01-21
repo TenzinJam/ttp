0ac11b20d637b5f3eb88356fb479b09adcab5285
Tenzin Jamyang <jamyangu@gmail.com>
2022-01-21 10:19:24 -0500

    form is updated. teacherForm

--- src/components/TeacherForm.js
+++ src/components/TeacherForm.js
@@ -16,11 +16,11 @@ export default function TeacherForm(){
       <form onSubmit={submitHandler}>
         <label>
           First Name:
-          <input type="text" name="firstName" value={firstName} onChange={() =>setFirstName(e.target.value)}/>
+          <input type="text" name="firstName" value={firstName} onChange={(e) =>setFirstName(e.target.value)}/>
         </label>
         <label>
           Last Name:
-          <input type="text" name="lastName" value={lastName} onChange={() => setLastName(e.target.value)}/>
+          <input type="text" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
